58845c0367d10bdcb200de3bb6fd56949ef90de0
Kasia Iwańska <sobylmine@gmail.com>
2016-11-18 19:24:42 +0100

    Add clearing all done tasks

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ class App extends Component {
         state: 'to do'
       },
       {
+        name: 'podlewanie kwiatków',
+        state: 'done'
+      },
+      {
         name: 'sprzątanie',
         state: 'to do'
       },
@@ -29,6 +33,14 @@ class App extends Component {
     ]
   }
 
+  clearDone = () => {
+    const { tasks } = this.state;
+    const tasksToDo = tasks.filter(({ state }) => state === 'to do');
+    this.setState({
+      tasks: [...tasksToDo]
+    })
+  }
+
   addTask = (name) => {
     const { tasks } = this.state;
     const newTask = {name, 
@@ -38,9 +50,15 @@ class App extends Component {
     })
   }
 
+  changeToDone = (event) => {
+    console.log('kasia');
+  }
+
   render() {
 
     const { tasks } = this.state;
+
+    // console.log(this.state[tasks[]])
 
     return (
       <div className="App">
@@ -50,7 +68,7 @@ class App extends Component {
         <div>
           <div className="list">
             <h3>TO DO:</h3>
-            <ToDo tasks={tasks} />
+            <ToDo tasks={tasks} onButtonClick={this.changeToDone} />
           </div>
           <div className="list">
             <h3>DONE:</h3>
@@ -61,7 +79,7 @@ class App extends Component {
           <h4>ADD NEW TO-DO TASK:</h4>
           <Form onFormSubmit={this.addTask} />
           <h4> CLEAR ALL DONE TASKS:</h4>
-          <button>Clear</button>
+          <button onClick={this.clearDone} >Clear</button>
         </div>
       </div>
     );

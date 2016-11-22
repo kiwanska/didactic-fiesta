b564b766920cc54021d551c058b994febdea1e5e
Kasia Iwańska <sobylmine@gmail.com>
2016-11-22 22:37:03 +0100

    Fix fetching data

--- wheather-check/src/App.js
+++ wheather-check/src/App.js
@@ -5,26 +5,27 @@ import '../css/style.css';
 class App extends Component {
 
   state = {
-  }
+    weather: {
 
-  fetchData = () => {
-    return fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=7ef88dd0d7db23ba7ca07e68793f8cd3', {'mode': 'no-cors'}).then(response =>
-        response.json().then(json => {
-          return json;
-        })
-      );
+    }
   }
 
   componentDidMount() {
-    // const data = fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=7ef88dd0d7db23ba7ca07e68793f8cd3', {'mode': 'no-cors'})
-    //   .then(response => response.json().then(json => { return json});
-
-      const data = this.fetchData();
-      console.log(data);
+    const data = fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=7ef88dd0d7db23ba7ca07e68793f8cd3')
+      .then(response => response.json().then(weather => {
+          this.setState({ weather });
+        }).catch((err) => console.log('ERROR', err)));
   }
 
 
   render() {
+    const { weather } = this.state;
+    console.log(weather);
+    const desc = (weather.weather) &&  weather.weather[1].description;
+    const iconUrl = (weather.weather) &&  `http://openweathermap.org/img/w/${weather.weather[1].icon}.png`;
+    const city = (weather.weather) &&  weather.name;
+
+    console.log(city);
 
     return (
       <div className="App">
@@ -32,7 +33,9 @@ class App extends Component {
           <img src={logo} className="react-logo" alt="logo" />
         </div>
         <div className="content">
-          <p>Hello World</p>
+          <p>Hello!</p>
+          <p className="desc">Oh! You can experience {desc} in {city} now.</p>
+          <img src={iconUrl}></img>
         </div>
       </div>
     );
